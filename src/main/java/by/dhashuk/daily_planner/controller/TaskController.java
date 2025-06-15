1badedebb54b74975ece69e3b4257ebbb07454f4
dgashuk <dmitrij.gashuk@gmail.com>
2025-06-15 12:14:06 +0300

    dailyTask controller added to code

--- src/main/java/by/dhashuk/daily_planner/controller/TaskController.java
+++ src/main/java/by/dhashuk/daily_planner/controller/TaskController.java
@@ -2,16 +2,61 @@ package by.dhashuk.daily_planner.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import by.dhashuk.daily_planner.entity.DailyTask;
+import by.dhashuk.daily_planner.service.DaskTaskService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 @RestController
 public class TaskController {
-    @GetMapping("/todolist")
-    public List<DailyTask> todoDailyList() {
-        return null;
+    @Autowired
+    public DaskTaskService dailyService;
+
+    @GetMapping("/dailytask")
+    public List<DailyTask> todoAllDailyList() {
+        //TODO add pagination
+        return dailyService.getAllDailyList();
     }
+
+    @GetMapping("/dailytask/{id}")
+    public DailyTask geDailyTaskbyId(@PathVariable String id) {
+        return dailyService.getTaskById(id);
+    }
+
+    @PostMapping("/dailytask")
+    public String postMethodName(@RequestBody String entity) {
+        //TODO: process POST request
+        throw new UnsupportedOperationException("Create task Unsupported!");
+    }
+
+    @PutMapping("/dailytask/{id}")
+    public String updateDailyTask(@PathVariable String id, @RequestBody String entity) {
+        //TODO: process PUT request
+        throw new UnsupportedOperationException("Update all task Unsupported!");
+    }
+
+    @PatchMapping("/dailytask/{id}")
+    public DailyTask updatePartOfDailyTask(@PathVariable String id){
+        //TODO: process Patch request
+        throw new UnsupportedOperationException("Update part of task Unsupported!");
+    }
+
+    @DeleteMapping("/dailytask/{id}")
+    public void removeTask(@PathVariable String id){
+        //TODO: process Delete request
+        throw new UnsupportedOperationException("Remove task Unsupported!");
+    }
+    
     
 }
--- src/main/java/by/dhashuk/daily_planner/entity/DailyTask.java
+++ src/main/java/by/dhashuk/daily_planner/entity/DailyTask.java
@@ -1,8 +1,14 @@
 package by.dhashuk.daily_planner.entity;
 
+import java.time.Duration;
 import java.time.LocalTime;
 
-import ch.qos.logback.core.util.Duration;
+import org.springframework.boot.context.properties.bind.Name;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +20,7 @@ public class DailyTask   {
     private String id;
     private String name;
     private String description;
+    @JsonProperty("duration")
     private Duration estimatedDuration;
     private LocalTime plannedTime;
     private boolean isCompleted;
